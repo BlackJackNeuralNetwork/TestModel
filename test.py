0609fbaea777f99647cb9b3290f7ff27fb9391b6
KaitoSatta <kait4apple.929@gmail.com>
2018-06-08 16:41:20 +0900

    initial commit

---
+++ test.py
@@ -0,0 +1,76 @@
+import numpy as np
+
+
+class Sigmoid:
+    def __init__(self):
+        self.out = None
+
+    def forward(self, x):
+        out = 1 / (1 + np.exp(-x))
+        self.out = out
+
+        return out
+
+    def backward(self, dout):
+        dx = dout * (1.0 - self.out) * self.out
+
+        return dx
+
+
+class Affine:
+    def __init__(self, w, b):
+        self.w = w
+        self.b = b
+        self.x = None
+        self.dw = None
+        self.db = None
+
+    def forward(self, x):
+        self.x = x
+        out = np.dot(x, self.w.T)
+
+        return out
+
+    def backward(self, dout):
+        dx = np.dot(dout, self.w.T)
+        self.dw = np.dot(self.x.T, dout)
+        self.db = np.sum(dout, axis=0)
+
+        return dx
+
+
+def cross_entropy_error(y, t):
+    if y.dim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+
+        batch_size = y.shape[0]
+        return -np.sum(t * np.log(y + 1 * np.e - 7)) / batch_size
+
+
+def softmax(a):
+    exp_a = np.exp(a)
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+
+    return y
+
+
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.loss = None
+        self.y = None
+        self.t = None
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = softmax(x)
+        self.loss = cross_entropy_error(self.y, self.t)
+
+        return self.loss
+
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+        dx = (self.y - self.t) / batch_size
+
+        return dx
